1625273ff76af4776bc0b4f467e8330572a6cedf
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-07-12 16:25:51 +0530

    updation2

--- app/home-page/page.tsx
+++ app/home-page/page.tsx
@@ -23,18 +23,20 @@ export default async function HomePage() {
         <p>Welcome, {formatUsernameAsName(email)}</p>
       </div>
       <br />
-      {
-        Tooldata.map((tool, index) => (
-          <ToolCard
-            key={index}
-            urlPath={tool.urlPath}
-            icon={tool.icon}
-            title={tool.title}
-            image={tool.image}
-            descriprion={tool.description}
-          />
-        ))
-      }
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-4 w-full">
+        {
+          Tooldata.map((tool, index) => (
+            <ToolCard
+              key={index}
+              urlPath={tool.urlPath}
+              icon={tool.icon}
+              title={tool.title}
+              image={tool.image}
+              descriprion={tool.description}
+            />
+          ))
+        }
+      </div>
     </section>
   );
 }
